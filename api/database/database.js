63d57341f79c427e34225f584bfed7b86eb28953
Manuel Andaya <mdandaya@gmail.com>
2021-02-18 19:56:37 -0800

    conditional config

--- api/database/database.js
+++ api/database/database.js
@@ -1,26 +1,30 @@
 const { Pool } = require('pg');
 
-// import development config file
 try {
   const config = require('./config');
+  const {
+    db: { host, database, user, port, password },
+  } = config;
+
+  const pool = new Pool({
+    host: host,
+    database: database,
+    user: user,
+    port: port,
+    password: password,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  module.exports = pool;
 } catch (ex) {
   console.log('no config file');
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  module.exports = pool;
 }
-
-const {
-  db: { host, database, user, port, password },
-} = config;
-
-const pool = new Pool({
-  host: host,
-  database: database,
-  user: user,
-  port: port,
-  password: password,
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-module.exports = pool;
